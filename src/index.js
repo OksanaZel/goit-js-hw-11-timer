c049796fdc88cbe1ef1905314abf27521fd07bb9
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-05-12 01:13:11 +0300

    Добавялет решение

--- src/index.js
+++ src/index.js
@@ -1,53 +1,140 @@
 import './main.scss';
 
-// const dateNow = Date.now();
-// console.log(dateNow);
+const refs = {
+  dateValue: document.querySelector('[data-value="days"]'),
+  hoursValue: document.querySelector('[data-value="hours"]'),
+  minsValue: document.querySelector('[data-value="mins"]'),
+  secsValue: document.querySelector('[data-value="secs"]'),
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]')
+}
 
-const timer = {
+class Timer {
+  constructor({ targetDate, onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+    this.targetDate = targetDate;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
   start() {
-    const targetDate = Date.now('May 13, 2021');
-    console.log(targetDate);
+    if (this.isActive) {
+      return;
+    }
 
-    setInterval(() => {
+    const startTime = this.targetDate;
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
       const currentTime = Date.now();
-      // console.log(currentTime);
-      console.log(targetDate - currentTime);
-    }, 1000)
+      const deltaTime = startTime - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+
+  stop() {
+      clearInterval(this.intervalId);
+      this.isActive = false;
+      const time = this.getTimeComponents(0);
+      this.onTick(time);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
   }
 }
 
+const timer = new Timer({
+  targetDate: new Date('May 12, 2021 01:10:00'),
+  onTick: updateClockface,
+});
+
 // timer.start();
 
-function getTimeComponents(time) {
-  /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  
-  return { days, hours, mins, secs };
+function updateClockface({ days, hours, mins, secs }) {
+  refs.dateValue.textContent = days;
+  refs.hoursValue.textContent = hours;
+  refs.minsValue.textContent = mins;
+  refs.secsValue.textContent = secs;
 }
 
-// new CountdownTimer({
+// class CountdownTimer {
+//   constuctor({ selector, targetDate, onTick }) {
+//     this.intervalId = null;
+//     this.selector = selector;
+//     this.targetDate = targetDate;
+//     this.onTick = onTick;
+    
+//   }
+
+//   start() {
+    
+//     this.intervalId = setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = this.targetDate - currentTime;
+
+//       const time = this.getTimeComponents(deltaTime);
+
+//       this.onTick(time);
+
+//     }, 1000)
+//   }
+
+//   // stop() {
+//   //   if (Date.now() > this.targetDate) {
+//   //     clearInterval(this.intervalId);
+//   //   }
+//   // }
+
+//     pad(value) {
+//       return String(value).padStart(2, '0');
+//     }
+    
+//     getTimeComponents(time) {
+//     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//     const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//     return { days, hours, mins, secs };
+// }
+// }
+
+// const countdownTimer = new CountdownTimer({
 //   selector: '#timer-1',
-//   targetDate: new Date('May 13, 2021'),
+//   targetDate: new Date('May 16, 2021'),
+//   onTick: updateSaleTimer,
 // });
+
+
+// function updateSaleTimer({ days, hours, mins, secs }) {
+//   refs.dateValue.textContent = days;
+//   refs.hoursValue.textContent = hours;
+//   refs.minsValue.textContent = mins;
+//   refs.secsValue.textContent = secs;
+// }
+
+// countdownTimer.start()
